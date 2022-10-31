c517791b456e92d883ee1b9e7cf6f4df8613e43a
hhamilton33 <56182992+hhamilton33@users.noreply.github.com>
2022-10-30 23:23:55 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const menu_icon = document.querySelector('.menu-icon');
-// const add_icon = document.querySelector('.add-icon');
 const mobile_menu = document.querySelector('#mobile-nav');
 
 menu_icon.addEventListener('click', () => {
     menu_icon.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 })
-
-// add_icon.addEventListener('click', () => {
-//     add_icon.classList.toggle('active');
-//     mobile_menu.classList.toggle('active');
-// })
 
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
     nav_menu.classList.remove('active');
