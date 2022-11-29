18e6fad2a47ca73738af85f9877741ff1a133686
hhamilton33 <56182992+hhamilton33@users.noreply.github.com>
2022-11-29 13:16:48 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -11,22 +11,4 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     mobile_menu.classList.remove('active');
 }))
 
-// $(document).ready(function() {
-//     $('ul li a').click(function() {
-//         $('li a').removeClass('active');
-//         $(this).addClass('active');
-
-//     });
-// });
-
-// function show_menu_open() {
-//     menu_icon.style.display = "block";
-//     add_icon.style.display = "none";
-// }
-
-// function show_menu_close() {
-//     add_icon.style.display = "block";
-//     menu_icon.style.display = "none";
-// }
-
 
